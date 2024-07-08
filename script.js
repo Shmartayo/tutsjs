3a799619dd39b8939e897eb297da8df5b8af7c1b
shmartayo <kunlexzy4@gmail.com>
2024-07-08 18:40:40 +0100

    Conditionals {Coin Toss}

---
+++ script.js
@@ -0,0 +1,19 @@
+/* 
+    Conditionals
+    Making decisions with code
+*/
+
+// let score = 70;
+
+// if (score > 90){
+//     console.log("You got A+");
+// } else {
+//     console.log("You did not get an A+");
+// }
+let coin = Math.random();
+
+if(coin >= 0.5){
+    console.log('Heads');
+} else {
+    console.log('Tails');
+}
