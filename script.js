e45b8adc81a01412f41b1d409684c11cf66e5171
shmartayo <kunlexzy4@gmail.com>
2024-07-08 22:05:16 +0100

    JavaScript Array Methods

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 /* 
-    Logical Operators- AND
-*/
+    ARRAYS - Ordered collections of values
+    ARRAY METHODS
+    push - adds to the end 
+    pop - removes from the end
+    shift -  remove from the front
+    unshift - add to the front
+    concat- merge arrays
+    includes - look for a value
+    indexOf - just like str.indexOf
+    join - crates a string from array
+    reverse - reverses an array
+    slice - copy portion of an array
+    splice - remove/replace elements in an array
+    sort - sorts an array
+    */
 
-let color = "purple";
-let animal = "toad";
+let nums = new Array(5,23,99,-10);
 
-if (color === "purple" && animal === "Toad" ){
-    console.log(`Yay ${color} ${animal}`);
-} else {
-    console.log(`BOO NOT ${color} ${animal} :(`);
-}
+nums.push(60);
+nums.push(80);
+nums.push(45);
+
+
+
+console.log(nums);
