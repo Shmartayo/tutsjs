0675db946cd31817efcba380316928008df5624a
shmartayo <kunlexzy4@gmail.com>
2024-07-09 23:12:15 +0100

    line 16: Recheck it

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-// Guessimg game
-let num = 7;
-let guess = prompt("Please Guess A Number!");
+// Guessing game
 
-while(guess !== num && guess <= 10){
-    if(guess < 7){
-        console.log(`${guess} IS TOO LOW!`);
-        guess = prompt("Please Guess A Number!");
-    } else if(guess > 7){
-        console.log(`${guess} IS TOO HIGH`);
-    } else {
-        alert("YOU GUESSED RIGHT");
+const startGame = () => {
+    let targetNum = generateRandom();
+    let guess = getInput();
+    while(guess !== targetNum) {
+        if(guess > targetNum){
+            console.log(`${guess} is too high`);
+        } else {
+            console.log(`${guess} is too low`);
+        }
+        /* 
+            Upwards would run an imfimite loop, to avoid that 
+            we meed a mew guess everytime we are loopimg
+        */
+        guess = parseInt(prompt("Please Guess Again!"));
+        while(Number.isNaN(guess)) {
+            guess = parseInt(prompt("Enter A  Valid Number!"));
+        }
     }
+    alert("You Win");
+}
+const generateRandom = () => {
+    return Math.floor(Math.random() * 10) + 1;
 }
 
+const getInput = () => {
+    let guess = parseInt(prompt('Enter a number'));
+    while (Number.isNaN(guess)) {
+        guess = parseInt(prompt("Please Enter a Valid Number"));
+    }
+    return guess;
+}
