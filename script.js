4c102eb8bcd0d4fb0e3ac3837223bef7cbcf0f59
shmartayo <kunlexzy4@gmail.com>
2024-07-09 19:39:24 +0100

    for loop to iterate and add an array and prompt guess the password game

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 /* Loops - Doing things repeatedly */
-for(let i = 0; i < 5; i++){
-    console.log(i);
+
+//Add the comtemts of am array
+const purchases = new Array(19.99,9.50,4.25,28.75,86.20,1.99,104.50,2.85);
+function addArray(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+
+        sum +=array[i];
+    }
+    console.log(sum);
 }
+
+addArray(purchases);
+
+
+// let password = 'lol';
+
+// let guess = prompt('Guess the password!');
+
+// while(guess !== password){
+//     console.log('Try again!')
+//     guess = prompt('Guess the password!');
+// }
+// console.log('You did it !!');
