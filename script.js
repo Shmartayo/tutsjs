bc37bf3a02e4998dc54d96c4a099201276932c20
shmartayo <kunlexzy4@gmail.com>
2024-07-21 16:34:57 +0100

    Profile Look Up from Nested objects inside an array- Coding challenge

--- script.js
+++ script.js
@@ -1,116 +1,64 @@
-//Iterate with While Loops
-//push the digits 0-4 into an array
+//Coding Challenge Profile Look Up
 
-let myArray = new Array();
-let i = 0;
-while(i<5){
-    myArray.push(i);
-    i++;
-}
+/* 
+    we pass in a name and a property and it's going to return
+    the value of that property,and if the contact does
+    not exist it should return "No such contact" and if the
+    contact exists and the property doesn't then it
+    should return "No such property"
+*/
 
-console.log("While loop: " + myArray);
-//push the digits 0-4 into an array
-
-let ourArray = new Array();
-for(let i= 0; i<5; i++){
-    ourArray.push(i);
-}
-
-console.log("For loop: " + ourArray);
-
-// Iterate Odd Numbers with a For Loop
-for(let i = 1; i < 10; i+=2){
-    console.log(i);
-}
-
-for(let i = 1; i<10; i++){
-    if(i%2 == 1){
-        console.log(i);
+let contacts = [
+    {
+        "firstName" : "Akira",
+        "LastName": "Laine",
+        "number":"0543236543",
+        "likes":["Pizza","Coding", "Brownie Points"]
+    },
+    {
+        "firstName" : "Harry",
+        "LastName": "Porter",
+        "number":"0994372684",
+        "likes":["Hogwarts","Magic", "Hagrid"]
+    },
+    {
+        "firstName" : "Sherlock",
+        "LastName": "Holmes",
+        "number":"0487345643",
+        "likes":["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName" : "Kristian",
+        "LastName": "Vos",
+        "number":"unknown",
+        "likes":["JavaScript","gaming", "Foxes"]
     }
-}
+    
+]
 
-// Iterate Even Numbers with a For Loop
-for(let i = 0; i<=10; i+=2){
-    console.log(i);
-}
+// function lookUpProfile(name, prop){
+//     for(let i = 0; i < contacts.length; i++){
+//         if(!contacts[i]["firstName"] == name){
+//             return "No such contact";
+//         } else {
+//             if(!contacts[i][prop]){
+//                 return "No such property";
+//             } else{
+//                 return contacts[i][prop];
+//             }
+//         }
+//     }
+// }
 
-//Count Backwards with a For Loop(even numbers)
-for(let i = 10; i > 0; i-= 2){
-    console.log(i);
-}
-
-let myarray = new Array();
-
-//Count Backwards with a For Loop(odd numbers)
-for(let i = 9; i > 0; i-= 2){
-    myarray.push(i);
-}
-console.log(myarray);
-
-let array = [1,2,3,4,5];
-
-//Iterate through an array with a For Loop
-for(let i = 0; i < array.length; i++){
-    console.log(myarray[i]);
-}
-
-// add all the elements of an array
-
-let arr =[1,2,3,4,5];
-let total = 0;
-
-for(let i = 0; i < arr.length; i++){
-    total +=arr[i];
-}
-
-console.log(total); // 15
-
-// Multiply all the elements in an array
-let arra = new Array(1,2,3,4,5);
-let mul = 1;
-
-for(let i = 0; i < arra.length; i++){
-    mul *= arra[i];
-}
-
-console.log(mul); // 120
-
-// Nesting For loops
-// if you have a multi-dimensional or nested array you can use 
-//  nested for loop to access all the array elements.
-// 2:22:36 
-
-function multiplyAll(arr){
-    let product = 1;
-
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr[i].length; j++){
-            product *= (arr[i][j]);
+function lookUpProfile(name,prop){
+    for(let i = 0; i <contacts.length; i++){
+        // Check if the name is a name in the contact list
+        if(contacts[i]["firstName"] === name){
+            return contacts[i][prop]|| "No such operator";
         }
     }
-
-    return product;
-
+    return "No such contact";
 }
 
-let product= multiplyAll([[1,2],[3,4],[5,6,7]]);
-console.log(product); // 5040
-
-// Accessing contents inside a nested array
-function accessNestedArray (arr){
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr[i].length; j++){
-            console.log(arr[i][j]);
-        }
-    }
-}
-
-accessNestedArray([[1,2],[3,4],[5,6,7]]);
-
-// Iterate with Do... While Loops
-let i = 0;
-
-do {
-    console.log("Ayo");
-    i++;
-} while (i < 5);
+let data = lookUpProfile("Akira", "likes");
+console.log(data);
