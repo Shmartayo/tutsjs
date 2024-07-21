0511b72ee2a47d1625b8ca5c62fc9110c3c784a5
shmartayo <kunlexzy4@gmail.com>
2024-07-21 15:56:21 +0100

    Loops in JavaScript

--- script.js
+++ script.js
@@ -54,7 +54,63 @@ for(let i = 0; i < array.length; i++){
     console.log(myarray[i]);
 }
 
+// add all the elements of an array
+
+let arr =[1,2,3,4,5];
+let total = 0;
+
+for(let i = 0; i < arr.length; i++){
+    total +=arr[i];
+}
+
+console.log(total); // 15
+
+// Multiply all the elements in an array
+let arra = new Array(1,2,3,4,5);
+let mul = 1;
+
+for(let i = 0; i < arra.length; i++){
+    mul *= arra[i];
+}
+
+console.log(mul); // 120
+
 // Nesting For loops
 // if you have a multi-dimensional or nested array you can use 
-//  nested for loop to access the elements in the array.
+//  nested for loop to access all the array elements.
 // 2:22:36 
+
+function multiplyAll(arr){
+    let product = 1;
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+            product *= (arr[i][j]);
+        }
+    }
+
+    return product;
+
+}
+
+let product= multiplyAll([[1,2],[3,4],[5,6,7]]);
+console.log(product); // 5040
+
+// Accessing contents inside a nested array
+function accessNestedArray (arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+            console.log(arr[i][j]);
+        }
+    }
+}
+
+accessNestedArray([[1,2],[3,4],[5,6,7]]);
+
+// Iterate with Do... While Loops
+let i = 0;
+
+do {
+    console.log("Ayo");
+    i++;
+} while (i < 5);
