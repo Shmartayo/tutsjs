e9d11b80ff47609d17cc96ac3bd6bd983f94b37f
shmartayo <kunlexzy4@gmail.com>
2024-07-22 11:41:13 +0100

    Use the Conditional (Ternary) Operator

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 /*
-    Use the parseInt() Function
-    Which takes a string and converts it to an integer
+    Use the Conditional (Ternary) Operator
+    it's like a one line if else conditional statement
 */
-function convertToInteger(str){
-    return parseInt(str);
+// Check if two values are equal
+
+function checkEqual(a,b){
+    return a==b ? true : false;
 }
 
-convertToInteger("5");
+checkEqual(1,1);
 
+// A better way to write a equals function is
+function checkEqual(a,b){
+    return a===b;
+}
+
+/* Using Multiple Conditional (Ternary) Operators
+    You can nest them within each other which even gives them
+    more power
+*/
 /**
- * Use the parseInt Function with a Radix
- * A Radix specifies the base of a number in a string such as
- * base 2, base 7, etc.
+ * checkSign function
+ * It's going to return the string "Positive" if the num that's passed into it is positive
+ *  "Negative" if the num that's passed into it is negative
+ * or Zero if the value passed in is zero
+ * 
  */
 
-function convertToIntegerFromBinary(str){
-    return parseInt(str,2);
+function checkSign(num){
+    return num > 0 ? "Positive" : num < 0 ? "negative" : num === 0 ? "Zero": "Nan";
 }
-
-convertToIntegerFromBinary("10011");
-
+checkSign(-1);
