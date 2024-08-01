37eef24b85dc1e28cb40d67fcc2646567e2af4a5
shmartayo <kunlexzy4@gmail.com>
2024-08-01 11:12:06 +0100

    Scope of let & var in JS

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-/*
-    Use the Conditional (Ternary) Operator
-    it's like a one line if else conditional statement
-*/
-// Check if two values are equal
-
-function checkEqual(a,b){
-    return a==b ? true : false;
-}
-
-checkEqual(1,1);
-
-// A better way to write a equals function is
-function checkEqual(a,b){
-    return a===b;
-}
-
-/* Using Multiple Conditional (Ternary) Operators
-    You can nest them within each other which even gives them
-    more power
-*/
-/**
- * checkSign function
- * It's going to return the string "Positive" if the num that's passed into it is positive
- *  "Negative" if the num that's passed into it is negative
- * or Zero if the value passed in is zero
+/**Difference between let and var
+ * Let does not allow you define a variable twice in the same scope
+ *  "use strict" - This enables strict mode in writing javaScript code
+ *  which catches common coding mistakes and unsafe actions
+ * (alot of people use "use strict" in a whole javaScript file or inside a function
+ * to catch coding mistakes)
  * 
- */
+*/ 
 
-function checkSign(num){
-    return num > 0 ? "Positive" : num < 0 ? "negative" : num === 0 ? "Zero": "Nan";
+/** Compare Scopes of the var and let Keywords
+ * When you declare a variable with "var" it is declared globally
+ * Or locally when it's declared inside a function
+ * However, the scope of let is limited to the block statement
+ * or expression
+*/
+// var keyword scope
+function checkScope(){
+    "use Strict"
+    let i = "function Scope";
+    if(true){
+        i = "block Scope";
+        console.log("Block Scope i is: " , i);
+    }
+    console.log("Function Scope i is: ", i);
+    return i;
 }
-checkSign(-1);
+
+checkScope();
+
+//let keyword scope 
+function checkScope(){
+    "use Strict"
+    let i = "function Scope";
+    if(true){
+        let i = "block Scope";
+        console.log("Block Scope i is: " , i);
+    }
+    console.log("Function Scope i is: ", i);
+    return i;
+}
+
+checkScope();
