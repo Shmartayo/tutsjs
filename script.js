e6f801207b93d1ccbd390cf5b7cc94913d94f84b
shmartayo <kunlexzy4@gmail.com>
2024-08-02 11:54:35 +0100

    Use the Rest Operator with Function Parameters

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-/*      <<<== Write Higher Order Anonymous Functions ==>>>
-  In order to create more flexible functions you can use default
-  parameters.
-  The default parameter kicks in when the argument is not specified
-  /is undefined
+/*    <<<== Use the Rest Operator with Function Parameters==>>>
+  The rest operator allows you to create a function that takes
+  a variable number of arguments.
+  The rest operator is represented by 3 dots(...)
 */
 
-const increment = (num, val = 1) => {
-  return num + val;
-}
+// A function that sums 3 arguments
+// const sum = (function() {
+//   return function sum(x,y,z) {
+//     const args = [x,y,z];
+//     return args.reduce((a,b) => a + b, 0);
+//   };
+// }) ();
+// console.log(sum(1,2,3));
 
-console.log(increment(5,2));
-console.log(increment(5));
-
+const sum = (function() {
+  return function sum(...args) {
+    return args.reduce((a,b) => a + b, 0);
+  };
+}) ();
+console.log(sum(1,2,3));
