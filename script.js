46686dab260b063d5f325577977c2770a3efa2f0
shmartayo <kunlexzy4@gmail.com>
2024-08-03 13:22:52 +0100

    Template Literal

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-/*<<<== Use Destructuring Assignment to pass an Object 
-  as a Function's Parameters ==>>>
-  This is commonly used with API calls when you are getting
-  information from an API request, you can use Destructuring
-  to get the elements you want.
-  
-*/
-const stats = {
-  max: 56.78,
-  standard_deviation: 4.34,
-  median: 34.54,
-  mode: 23.87,
-  min: -0.75,
-  average: 35.85
+// template literals
+const person = {
+  name : "Zodiac Hasbro",
+  age: 56
 };
 
-const half = (function() {
-
-  return function half({max, min}) {
-    return (max + min) / 2.0
-  };
-
-})();
-console.log(stats);
-console.log(half(stats));
-
-//Previous way to pass an object into a function - but this method passes all the elements inside a function
-// const halfs = (function(){
-//   return function half(stats){
-//     return (stats.max + stats.min) /2.0;
-//   }
-// }) ();
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+console.log(greeting);
