b69365862aa95ec29f1bb45b967c1920a6c91c6d
shmartayo <kunlexzy4@gmail.com>
2024-08-03 10:38:55 +0100

    intermission

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-/*<<<== Use Destructuring Assignment to Assign Variables from Arrays ==>>>
+/*<<<== Use Destructuring Assignment with the Rest Operator ==>>>
   We can also use Destructuring assignment to assign 
   variables from Arrays.
 */
-var [z, x] = [1, 2, 3, 4, 5, 6];
-console.log(z, x); // The difference between Destructuring from an array and destructuring from objects is that you cannot specify which element of the array is going into a variable, it just goes in order
-
-// However if we want the number 4 and 6 to go into the array
-
-var [z, x, , y, , a] = [1, 2, 3, 4, 5, 6];
-console.log(z, x, y, a);
-
-/**
- * You can use the Destructuring of arrays to switch
- * the places  of variables
-*/
-
-let a = 8, b = 6;
-(() =>{
-  "use strict";
-  [a, b] = [b, a];
-})();
-console.log(a,b);
