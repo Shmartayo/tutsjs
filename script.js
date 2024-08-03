0ea2761104602dd1e45d4a3a2c43abe450f16d28
shmartayo <kunlexzy4@gmail.com>
2024-08-03 10:35:57 +0100

    Use Destructuring Assignment to Assign Variables from Arrays

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-/*  <<<== Use Destructuring Assignment with Nested Objects ==>>>
-We can also use Destructuring assignment operator to assign variables of nested objects
+/*<<<== Use Destructuring Assignment to Assign Variables from Arrays ==>>>
+  We can also use Destructuring assignment to assign 
+  variables from Arrays.
+*/
+var [z, x] = [1, 2, 3, 4, 5, 6];
+console.log(z, x); // The difference between Destructuring from an array and destructuring from objects is that you cannot specify which element of the array is going into a variable, it just goes in order
+
+// However if we want the number 4 and 6 to go into the array
+
+var [z, x, , y, , a] = [1, 2, 3, 4, 5, 6];
+console.log(z, x, y, a);
+
+/**
+ * You can use the Destructuring of arrays to switch
+ * the places  of variables
 */
 
-const LOCAL_FORECAST = {
-  today: {min:72, max:83},
-  tomorrow: {min:73.3, max:84.6}
-};
-
-function getMaxOfTmrw(forecast){
+let a = 8, b = 6;
+(() =>{
   "use strict";
-  const {tomorrow : { max : maxOfTomorrow }} = forecast;
-  return maxOfTomorrow;
-}
-
-console.log(getMaxOfTmrw(LOCAL_FORECAST));
+  [a, b] = [b, a];
+})();
+console.log(a,b);
